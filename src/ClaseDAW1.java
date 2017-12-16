5a50b12f41ab3d52009541b7e85493c12abdf2f3
victor <victor@victor-PC>
2017-12-16 13:31:07 +0100

    Este es mi primer repositorio

---
+++ src/ClaseDAW1.java
@@ -0,0 +1,32 @@
+ 
+public class ClaseDAW1 {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		String profesor = "Luis de Blas";
+		String al01_AdrianPosadas ="";
+		String al02_SergioArrey="";
+		String al03_AndreiAvasiloaiei ="";
+		String al04_AlexBerodia = "";
+		String al05_CristinaBlanco = "Cris Blanco";
+		String al06_BarbaraBolado = "";
+		String al07_IvanCasado = "Iván Casado";
+		String al08_RubenCayon = "";
+		String al09_PedroDelgado = "";
+		String al10_DarioEscobar = "";
+		String al11_PaulaEstrada = "Paula :) ";
+		String al13_ManuelGarcia = "";
+		String al14_MarioGomez = "";
+		String al15_ManuelGonzalez = "";
+		String al16_CeciliaHerrero = "";
+		String al18_AlfredoMarcano = "";
+		String al20_OmarNieto = "";
+		String al21_RaquelPerez = "Raquel P. Bonifaz";
+		String al22_VictorPernia = "Victor P Calderon";
+		String al23_JavierRubio = "";
+		String al25_CristianVartic = "";
+		String al26_AlvaroDiez = "";
+
+	}
+
+}
